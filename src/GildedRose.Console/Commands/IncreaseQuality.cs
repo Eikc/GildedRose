624d1e0c5ccc780a8984d63e78a96b9daf1ca573
Eik Emil Christensen <me@eikc.dk>
2018-01-29 23:11:51 +0100

    added increase quality command to handle logic around increasing quality

---
+++ src/GildedRose.Console/Commands/IncreaseQuality.cs
@@ -0,0 +1,18 @@
+﻿namespace GildedRose.Console.Commands
+{
+    public interface IIncreaseQuality
+    {
+        void Execute(Item item);
+    }
+
+    public class IncreaseQuality : IIncreaseQuality
+    {
+        public void Execute(Item item)
+        {
+            if (item.Quality >= 50)
+                return;
+
+            item.Quality++;
+        }
+    }
+}
---
+++ src/GildedRose.Tests/Commands/IncreaseQualityTests.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using GildedRose.Console;
+using GildedRose.Console.Commands;
+using Xunit;
+
+namespace GildedRose.Tests.Commands
+{
+    public class IncreaseQualityTests
+    {
+        private readonly IncreaseQuality _sut;
+
+        public IncreaseQualityTests()
+        {
+            _sut = new IncreaseQuality();
+        }
+
+        [Theory]
+        [InlineData(50)]
+        [InlineData(60)]
+        [InlineData(80)]
+        public void DoesNotIncreasesQualityWhenItemIs50OrAbove(int quality)
+        {
+            var item = new Item { Name = "+5 Dexterity Vest", SellIn = 5, Quality = quality };
+
+            _sut.Execute(item);
+
+            item.Quality.ShouldBeEquivalentTo(quality);
+        }
+
+        [Fact]
+        public void IncreasesQualityWhenItemIsBelow50()
+        {
+            var item = new Item { Name = "+5 Dexterity Vest", SellIn = 5, Quality = 10 };
+
+            _sut.Execute(item);
+
+            item.Quality.ShouldBeEquivalentTo(11);
+        }
+    }
+}
