c5f8b44d1dcc4d52339ed2b6515f175b82d05211
Eik Emil Christensen <me@eikc.dk>
2018-01-29 23:09:55 +0100

    added decrease quality command to handle decreasing quality

---
+++ src/GildedRose.Console/Commands/DecreaseQuality.cs
@@ -0,0 +1,18 @@
+﻿namespace GildedRose.Console.Commands
+{
+    public interface IDecreaseQuality
+    {
+        void Execute(Item item);
+    }
+
+    public class DecreaseQuality : IDecreaseQuality
+    {
+        public void Execute(Item item)
+        {
+            if (item.Quality <= 0)
+                return;
+
+            item.Quality--;
+        }
+    }
+}
