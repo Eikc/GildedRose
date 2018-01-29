04118e0bdb4cea7264bf07e39a2ff8c1d5f460f5
Eik Emil Christensen <me@eikc.dk>
2018-01-30 00:29:10 +0100

    replaced old implementation with new command/query based implementation

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -1,37 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
+using GildedRose.Console.Commands;
 
 namespace GildedRose.Console
 {
     public class Program
     {
         private readonly IList<Item> _items;
+        private readonly IUpdateInventory _updateInventory;
 
-        public Program(IList<Item> items)
+        public Program(IList<Item> items, IUpdateInventory updateInventory)
         {
             _items = items;
+            _updateInventory = updateInventory;
         }
 
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
+            var updateAgedBrie = new UpdateAgedBrie(new IncreaseQuality(), new DecreaseSellIn());
+            var updateBackStageItems = new UpdateBackStageItems(new IncreaseQuality(), new DecreaseSellIn());
+            var updateOrdinaryItems = new UpdateOrdinaryItem(new DecreaseQuality(), new DecreaseSellIn());
+            var updateConjuredItems = new UpdateConjuredItems(new DecreaseQuality(), new DecreaseSellIn());
+
+            var updateInventory = new UpdateInventory(updateBackStageItems, updateAgedBrie, updateConjuredItems,
+                updateOrdinaryItems);
 
 
             var app = new Program(new List<Item>
-                                          {
-                                              new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                                              new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                                              new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                                              new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                                              new Item
-                                                  {
-                                                      Name = "Backstage passes to a TAFKAL80ETC concert",
-                                                      SellIn = 15,
-                                                      Quality = 20
-                                                  },
-                                              new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                                          });
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                },
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            }, updateInventory);
 
             app.UpdateQuality();
 
@@ -41,92 +50,11 @@ namespace GildedRose.Console
 
         public void UpdateQuality()
         {
-            foreach (var item in _items.Where(x => !x.IsSulfurasTheHandOfRagnaros()))
-            {
-                AdjustQualityOf(item);
-                DecreaseSellInFor(item);
-                DecreaseQualityOfOldItem(item);
-            }
-        }
 
-        private static void DecreaseQualityOfOldItem(Item item)
-        {
-            if (item.SellIn >= 0) return;
-
-            if (!item.IsAgedBrie() && !item.IsBackStagePass())
-            {
-                if (item.Quality <= 0) return;
-
-                item.Quality = item.Quality - 1;
-
-                if (item.IsConjured())
-                {
-                    item.Quality--;
-                }
-            }
-            else if (item.IsBackStagePass())
-            {
-                item.Quality = item.Quality - item.Quality;
-            }
-            else
-            {
-                if (item.Quality < 50)
-                {
-                    item.Quality = item.Quality + 1;
-                }
-            }
-        }
-
-        private static void DecreaseSellInFor(Item item)
-        {
-            item.SellIn--;
-        }
-
-        private static void AdjustQualityOf(Item item)
-        {
-            if (item.IsConjured())
-            {
-                item.Quality--;
-            }
-
-            if (item.IsAgedBrie() || item.IsBackStagePass())
-            {
-                IncreaseBrieAndBackStage(item);
-                return;
-            }
-
-            if (item.Quality > 0)
-            {
-                item.Quality--;
-            }
-        }
-
-        private static void IncreaseBrieAndBackStage(Item item)
-        {
-            if (item.Quality >= 50) return;
-
-            item.Quality = item.Quality + 1;
-
-            if (item.IsBackStagePass())
-            {
-                if (item.SellIn < 11)
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-                }
-
-                if (item.SellIn < 6)
-                {
-                    if (item.Quality < 50)
-                    {
-                        item.Quality = item.Quality + 1;
-                    }
-                }
-            }
+            _updateInventory.Execute(_items);
         }
     }
+
 
     public class Item
     {
