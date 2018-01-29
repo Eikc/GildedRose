2a03e53a08e8c341dc8ab3f9471ad7088c6e2ba0
Eik Emil Christensen <me@eikc.dk>
2018-01-29 23:44:33 +0100

    added UpdateAgedBrie command to handle logic surrounding Aged Brie
    adjusted the test to use a better method
    
    
    added missing csproj file

---
+++ src/GildedRose.Console/Commands/UpdateAgedBrie.cs
@@ -0,0 +1,37 @@
+﻿namespace GildedRose.Console.Commands
+{
+    public interface IUpdateAgedBrie
+    {
+        void Execute(Item item);
+    }
+
+    public class UpdateAgedBrie : IUpdateAgedBrie
+    {
+        private readonly IIncreaseQuality _increaseQuality;
+        private readonly IDecreaseSellIn _decreaseSellIn;
+
+        public UpdateAgedBrie(
+            IIncreaseQuality increaseQuality,
+            IDecreaseSellIn decreaseSellIn
+        )
+        {
+            _increaseQuality = increaseQuality;
+            _decreaseSellIn = decreaseSellIn;
+        }
+
+        public void Execute(Item item)
+        {
+            if (!item.IsAgedBrie())
+                return;
+
+            _increaseQuality.Execute(item);
+            _decreaseSellIn.Execute(item);
+
+            if (ItemIsToOld(item))
+                _increaseQuality.Execute(item);
+        }
+
+        private static bool ItemIsToOld(Item item)
+            => item.SellIn < 0;
+    }
+}
