2a6264a9a2033bee91c31bf022593c19a25ce0fc
Eik Emil Christensen <me@eikc.dk>
2018-01-30 00:20:23 +0100

    added command to handle logic surroudign ordinary items
    Fixed missing rename of interface

---
+++ src/GildedRose.Console/Commands/UpdateOrdinaryItem.cs
@@ -0,0 +1,39 @@
+﻿namespace GildedRose.Console.Commands
+{
+    public interface IUpdateOrdinaryItems
+    {
+        void Execute(Item item);
+    }
+
+    public class UpdateOrdinaryItem : IUpdateOrdinaryItems
+    {
+        private readonly IDecreaseQuality _decreaseQuality;
+        private readonly IDecreaseSellIn _decreaseSellIn;
+
+        public UpdateOrdinaryItem(IDecreaseQuality decreaseQuality, IDecreaseSellIn decreaseSellIn)
+        {
+            _decreaseQuality = decreaseQuality;
+            _decreaseSellIn = decreaseSellIn;
+        }
+
+        public void Execute(Item item)
+        {
+            if (ItemIsNotOrdinary(item))
+                return;
+
+            _decreaseQuality.Execute(item);
+            _decreaseSellIn.Execute(item);
+
+            if (ItemIsToOld(item))
+                _decreaseQuality.Execute(item);
+        }
+
+        private static bool ItemIsNotOrdinary(Item item)
+            => item.IsBackStagePass() ||
+               item.IsAgedBrie() ||
+               item.IsSulfurasTheHandOfRagnaros();
+
+        private static bool ItemIsToOld(Item item)
+            => item.SellIn < 0;
+    }
+}
